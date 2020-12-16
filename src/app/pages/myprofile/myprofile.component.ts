1799100f0f4e35009d683c9dce80ca8bbc20cd8e
Jay Mark David <jaymarkd@cloudstaff.com>
2020-12-16 10:43:47 +0800

    Forms

--- src/app/pages/myprofile/myprofile.component.ts
+++ src/app/pages/myprofile/myprofile.component.ts
@@ -1,6 +1,6 @@
-import { Profile } from './profile-model';
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Profile } from './profile-model';
 
 @Component({
   selector: 'app-myprofile',
@@ -9,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyprofileComponent implements OnInit {
 
+  profileForm: any ;
+
   constructor() { }
+
 
   profile: Profile = {
     email: '',
@@ -22,6 +25,24 @@ export class MyprofileComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.profileForm = new FormGroup({
+      email: new FormControl('',[Validators.required, Validators.email ]),
+      first_name: new FormControl('',[Validators.required]),
+      last_name: new FormControl('',[Validators.required]),
+      alias: new FormControl('',[Validators.required]),
+      job_title: new FormControl('',[Validators.required]),
+      mobile_number: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required]),
+      confirm_password: new FormControl('',[Validators.required]),
+    });
+  }
+
+  onSubmit(): void {
+    if(this.profileForm.valid){
+      console.log('can submit');
+    }
+    console.log('form is valid: ',this.profileForm.valid);
+    console.log('form values: ',this.profileForm.value);
   }
 
 }
